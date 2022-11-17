99fca5d488754f46810438f4579a71970897084d
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-11-17 17:58:12 +0530

    .

--- wordpressinstall1.sh
+++ wordpressinstall1.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 apt update
+sudo apt install nginx
+sudo apt install mariadb-server mariadb-client -y
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo mysql_secure_installation
+sudo apt install php8.1 php8.1-fpm php8.1-mysql php-common php8.1-cli php8.1-common php8.1-opcache php8.1-readline php8.1-mbstring php8.1-xml php8.1-gd php8.1-curl
