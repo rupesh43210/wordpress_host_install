e443a6da445e287e9cde692ff16592fffe1bbcd7
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-11-18 12:42:34 +0530

    .

--- 1_lemp_install.sh
+++ 1_lemp_install.sh
@@ -12,7 +12,7 @@ sudo systemctl start mariadb
 sudo systemctl enable mariadb
 sudo mysql_secure_installation
 #setup-php
-sudo apt install php8.1 php8.1-fpm php8.1-mysql php-common php8.1-cli php8.1-common php8.1-opcache php8.1-readline php8.1-mbstring php8.1-xml php8.1-gd php8.1-curl
+sudo apt install php8.1 php8.1-fpm php8.1-mysql php-common php8.1-cli php8.1-common php8.1-opcache php8.1-readline php8.1-mbstring php8.1-xml php8.1-gd php8.1-curl -y
 sudo systemctl start php8.1-fpm
 sudo systemctl enable php8.1-fpm
 sudo systemctl status php8.1-fpm
