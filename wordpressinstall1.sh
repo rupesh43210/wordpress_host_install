5a07d505db53e065e9b383abd5871029a08eb34e
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-11-18 11:20:07 +0530

    .

--- wordpressinstall1.sh
+++ wordpressinstall1.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 apt update
+
+#setup nginx web-server
 sudo apt install nginx
+sudo systemctl status nginx
+#setup database server and client
 sudo apt install mariadb-server mariadb-client -y
+sudo systemctl status mariadb
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 sudo mysql_secure_installation
+#setup-php
 sudo apt install php8.1 php8.1-fpm php8.1-mysql php-common php8.1-cli php8.1-common php8.1-opcache php8.1-readline php8.1-mbstring php8.1-xml php8.1-gd php8.1-curl
+sudo systemctl start php8.1-fpm
+sudo systemctl enable php8.1-fpm
+sudo systemctl status php8.1-fpm
+
+sed -i 's/^upload_max_filesize =*/upload_max_filesize = 32M' /etc/php/8.1/fpm/php.ini
+
+
+sudo systemctl restart php8.1-fpm
