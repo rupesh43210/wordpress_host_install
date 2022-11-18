4735d080f35b81318d83a17cdbcf1f03bb592600
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-11-18 12:51:57 +0530

    .

--- 1_lemp_install.sh
+++ 1_lemp_install.sh
@@ -63,8 +63,8 @@ echo "LEMP has been insalled successfully"
 read -r -p "Do you want to setup wordpress now: [y/N]?" permission
 
 if [[ "$permission" == Y || "$permission" == y ]]; then
-chmod +x 2_dbsetup
-./2_dbsetup.sh
+sudo chmod +x 2_dbsetup
+sudo ./2_dbsetup.sh
 else echo "Setup wordpress as and when required later"
 fi
 
