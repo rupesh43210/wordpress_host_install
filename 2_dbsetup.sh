332b1d454e32b20f4b046cd8a396a01e91a16b8c
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-11-18 18:24:51 +0530

    .

--- 2_dbsetup.sh
+++ 2_dbsetup.sh
@@ -185,7 +185,8 @@ setupssl(){
 													echo "generating and signing certificates"
 													openssl req -x509 -newkey rsa:"$rsa_value" -nodes -out $public_certificate_path -keyout $private_key_path -days "$certificate_duration_in_days" -subj "/C=$country_code/O=$organization_name/OU=$organizational_unit/CN=$common_name"
 
-													
+													sudo systemctl reload nginx
+													echo "Everything done Please go to your website"
 												
 												elif [[ $REPLY == "2" ]]; then
 
@@ -214,6 +215,3 @@ setupssl(){
 
 setupssl
 
-sudo systemctl reload nginx
-
-echo "Everything done Please go to your website"
