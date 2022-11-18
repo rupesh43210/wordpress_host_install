f72d68b8c0f0c3b86d47a9a3d67b00d4e2aa842a
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-11-18 20:19:08 +0530

    .

--- 2_dbsetup.sh
+++ 2_dbsetup.sh
@@ -206,7 +206,7 @@ setupssl(){
 
 														read -r -p "Enter your email: " email
 
-														sudo certbot certonly --webroot --webroot-path /var/www/"$wordpress" -m "$email" -d www."$wordpress" -d "$wordpress" --agree-tos -n
+														sudo certbot certonly --webroot --webroot-path /var/www/"$wordpress" -m "$email"  -d "$wordpress" --agree-tos -n
 														sudo systemctl reload nginx
 
 														#sudo certbot --nginx -d "$common_name" -d www."$common_name"
