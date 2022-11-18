5333e60dfab3969379fbcbf86b90ef3ab2964f48
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-11-18 11:26:17 +0530

    .

--- wordpressinstall1.sh
+++ wordpressinstall1.sh
@@ -17,7 +17,11 @@ sudo systemctl start php8.1-fpm
 sudo systemctl enable php8.1-fpm
 sudo systemctl status php8.1-fpm
 
-sed -i 's/^upload_max_filesize =*/upload_max_filesize = 32M' /etc/php/8.1/fpm/php.ini
-
+sed -i 's/^upload_max_filesize = */upload_max_filesize = 32M' /etc/php/8.1/fpm/php.ini
+sed -i 's/^post_max_size = */post_max_size = 48M' /etc/php/8.1/fpm/php.ini
+sed -i 's/^memory_limit = */memory_limit = 256M' /etc/php/8.1/fpm/php.ini
+sed -i 's/^max_execution_time  =*/max_execution_time = 600' /etc/php/8.1/fpm/php.ini
+sed -i 's/^max_input_vars = */max_input_vars = 3000' /etc/php/8.1/fpm/php.ini
+sed -i 's/^max_input_time = */max_input_time = 1000' /etc/php/8.1/fpm/php.ini
 
 sudo systemctl restart php8.1-fpm
